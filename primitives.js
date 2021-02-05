884167822c710a82af6c42cad76767d14aeacff0
Kristianpeterzwart <kristianzwartdesign@gmail.com>
2021-02-05 16:15:10 +0100

    Update primitives.js
    use curly braces for console log to output multi lines

--- primitives.js
+++ primitives.js
@@ -7,4 +7,6 @@ let lastName = null // use to explicitly clear the value of a variable
 let selectedColor = 'red'
 let sym1 = Symbol() // returns value of type symbol
 
-console.log(name, age, isApproved, firstName, lastName)
+console.log({name, age, isApproved, firstName, lastName})
+
+// use curly braces to print out multi lines for console log
