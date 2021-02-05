6e40b63422ce481410436e534e24edf735bbab7d
Kristianpeterzwart <kristianzwartdesign@gmail.com>
2021-02-05 14:24:27 +0100

    Commit Variables

---
+++ variables.js
@@ -0,0 +1,13 @@
+
+// let name = 'Kris'
+// console.log(name)
+
+// Cannot be a reserved keyword
+// Should be meaningful
+// Cannot start with a number (1name)
+// Cannot contain a space or hyphen (-)
+// Are case-sensitive
+
+let firstName = 'Kristian'
+let lastName = 'Zwart'
+console.log(firstName, lastName)
