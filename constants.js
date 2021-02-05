0d8b011a3f78a2d48bc84543a283fff1b7d8c112
Kristianpeterzwart <kristianzwartdesign@gmail.com>
2021-02-05 14:54:41 +0100

    constants and variables
    Difference between let and constant
    
    Note - cannot reassign a constant
    If need to reassign a variable

---
+++ constants.js
@@ -0,0 +1,9 @@
+let interestRate = 0.3
+interestRate = 1;
+console.log(interestRate);
+
+// const interestRate = 0.3
+// interestRate = 1;
+// console.log(interestRate);
+
+// Real World - do not want the value of a variable to change
---
+++ primitives.js
@@ -0,0 +1,10 @@
+
+let name = 'Kris' // String Literal
+let age = 44 // Number Literal
+let isApproved = true // Boolean Literal - both true or false
+let firstName = undefined // Undefined
+let lastName = null // use to explicitly clear the value of a variable
+let selectedColor = 'red'
+let sym1 = Symbol() // returns value of type symbol
+
+console.log(name, age, isApproved, firstName, lastName)
