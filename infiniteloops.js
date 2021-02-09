6440e076008c8e8fb88e837c7f7765c4efb03ab3
Kristianpeterzwart <kristianzwartdesign@gmail.com>
2021-02-09 15:00:58 +0100

    updates

---
+++ infiniteloops.js
@@ -0,0 +1,17 @@
+
+let i = 0
+while (i < 5) {
+    console.log(i)
+    i++
+}
+
+// infinite loops are not limited to while loops
+
+while (true) {
+}
+
+do {
+ // x++
+} while (x < 5)
+
+for (let i = 0; i < 10; i++) {}
