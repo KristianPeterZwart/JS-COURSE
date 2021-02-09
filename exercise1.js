e34469426d1817c6b63f0f41014ae7cd4e4e39ac
Kristianpeterzwart <kristianzwartdesign@gmail.com>
2021-02-09 15:54:02 +0100

    Homework Assignment
    Create a function to return the max of two numbers.
    
    shorten and clean the code

--- exercise1.js
+++ exercise1.js
@@ -1,11 +1,16 @@
 // Swapping Variables
 
+// 
+
 let a = 'red'
 let b = 'blue'
 let c = a
+
 a = b
 b = c
 
+// 
 console.log(a)
 console.log(b)
 
+// 
---
+++ homework1.js
@@ -0,0 +1,17 @@
+
+// Exercise 1 - Max of Two Numbers
+let number = max(5, 10)
+console.log(number)
+
+function max(a, b) {
+    return (a > b) ? a : b
+}    
+ //   if (a > b) return a;
+ //   else return b;
+
+// firstly remove else
+ //   return b;
+
+//    (condition) ? a : // otherwise b
+
+//    return (a > b) ? a : b // this is better code
