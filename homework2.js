d750cd74555dc51b98b581342d90088ad339e54d
Kristianpeterzwart <kristianzwartdesign@gmail.com>
2021-02-09 16:13:29 +0100

    Create homework2.js

---
+++ homework2.js
@@ -0,0 +1,13 @@
+// Homework Assignment 2 - Create function to determine 
+// if Landscape or Portrait
+
+//True if landscape if width greater than height greater
+//otherwise height is greater equals portrait
+
+console.log(isLandscape(300, 600))
+
+function isLandscape(width, height) {
+    return (width > height) 
+}
+
+// Do not need to explicitly add true or false
