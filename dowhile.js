26a1a679c49f3e9d792e5cf336a5fb06ec5e5724
Kristianpeterzwart <kristianzwartdesign@gmail.com>
2021-02-09 15:01:21 +0100

    updates 2

---
+++ dowhile.js
@@ -0,0 +1,21 @@
+
+// to display odd numbers 0 - to 5
+
+// for (let i = 0; i <= 5; i++) {
+    // if (i % 2 !== 0) console.log(i) 
+// }
+
+let i = 9
+while (i <= 5) {
+if (i % 2 !== 0) console.log(i) 
+i++
+}
+
+// do-while
+let i = 0
+do {
+    if (i % 2 !== 0) console.log(i) 
+i++
+} while (i <= 5)
+
+// most of the time will be using for while loops instead of do while
---
+++ for-in.js
@@ -0,0 +1,29 @@
+
+// for
+// while
+// do-while loop
+
+// for-in
+const person = {
+    name: 'Kris',
+    age: 44
+};
+
+// for (initialExpression; condition; increment) // this 3 part for loop
+
+for (let key in person)
+    console.log(key, person[key]);
+
+
+    const colors = ['red', 'green', 'blue']
+
+for (let index in colors)
+    console.log(index, colors[index])
+
+// Dot Notation
+// person.name
+// 
+// Bracket Notation
+// person['name']
+// 
+// for -of
